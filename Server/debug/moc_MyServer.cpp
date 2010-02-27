10c240e60071a3f69bdbbd642c8de616fe07f90b
Alex <aizenbit@users.noreply.github.com>
2010-02-27 17:21:01 +0000

    Введен класс Messаge, не передаётся имя клиента, созданы коды

--- Server/debug/moc_MyServer.cpp
+++ Server/debug/moc_MyServer.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MyServer.h'
 **
-** Created: Sat 27. Feb 17:58:16 2010
+** Created: Sat 27. Feb 19:48:32 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.1)
 **
 ** WARNING! All changes made in this file will be lost!
