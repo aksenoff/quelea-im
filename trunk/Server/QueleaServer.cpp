4e6f1439a95f5464074bbbfa5d4c70c0f691ca99
romsuhov <romsuhov@128f5ee6-1e47-11df-9e47-91152886f6d8>
2010-09-26 16:56:30 +0000

    Добавлено отделение цветом в окне сообщений. Ещё не доработано, так как система сообщений требует доработки.

--- trunk/Server/QueleaServer.cpp
+++ trunk/Server/QueleaServer.cpp
@@ -180,7 +180,7 @@ void QueleaServer::slotReadClient()
                 {
                     QVector<Client*>::iterator i;
                     for(i=clients.begin();(*i)->getname()!=messtoserv[0];++i);
-                    QString nstr = " -> "+(*i)->getname();
+                    QString nstr = (*i)->getname();
                     str=(*from)->getname()+";"+messtoserv[1]+";"+nstr;
                     Message* newmess = new Message(MESSAGE_TO_CLIENT,str);
                     for (int u=0;u<clients.size();u++)
