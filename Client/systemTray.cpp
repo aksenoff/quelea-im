309e1f6e677200ab725b897aa2f63f1e0cc92219
aksenoff.a <aksenoff@users.noreply.github.com>
2011-01-17 21:46:32 +0000

    Устранены утечки памяти класса SystemTray. Частичный рефакторинг.

--- Client/systemTray.cpp
+++ Client/systemTray.cpp
@@ -1,138 +1,157 @@
 #include <QSystemTrayIcon>
-#include<QAction>
-#include<QMenu>
-#include<QApplication>
+#include <QAction>
+#include <QMenu>
+#include <QApplication>
 #include "systemTray.h"
 
-SystemTray::SystemTray(QWidget *pwgt /*=0*/, QueleaClient* client /*= 0*/)
-    : QWidget(pwgt),iconSwitcher(false), nMess(false),qc(client)
+SystemTray::SystemTray(QWidget *pwgt /*=0*/, QueleaClient* queleaClient /*= 0*/)
+    : QWidget(pwgt), nMess(false),client(queleaClient)
 {
+    connect(client, SIGNAL(statusChanged(QString)),
+            this, SLOT(slotChangeIcon(QString)));
+    connect(client, SIGNAL(newMessage(QString)),
+            this, SLOT(slotNewMessage(QString)));
 
-connect(qc,SIGNAL(statusChanged(QString)),this,SLOT(slotChangeIcon(QString)));
-connect(qc,SIGNAL(newMessage(QString)),this,SLOT(slotNewMessage(QString)));
+    actShowHide = new QAction(tr("Скрыть"),this);
+    connect(actShowHide, SIGNAL(triggered()),
+            this, SLOT(slotShowHide()));
 
-actShowHide = new QAction(tr("Скрыть"),this);
-connect(actShowHide,SIGNAL(triggered()),this,SLOT(slotShowHide()));
+    actChangeStatus = new QAction(tr("Подключиться"),this);
+    connect(actChangeStatus, SIGNAL(triggered()),
+            this, SLOT(slotChangeStatus()));
 
-actChangeStatus = new QAction(tr("Подключиться"),this);
-connect(actChangeStatus,SIGNAL(triggered()),this,SLOT(slotChangeStatus()));
+    actSettings = new QAction(tr("Настройки..."),this);
+    connect(actSettings, SIGNAL(triggered()),
+            client, SLOT(openSettingDialog()));
 
-actSettings = new QAction(tr("Настройки..."),this);
-connect(actSettings,SIGNAL(triggered()),qc,SLOT(openSettingDialog()));
+    actQuit = new QAction(tr("Выход"),this);
+    connect(actQuit, SIGNAL(triggered()),
+            qApp, SLOT(quit()));
 
-actQuit = new QAction(tr("Выход"),this);
-connect(actQuit,SIGNAL(triggered()),qApp,SLOT(quit()));
+    trayIconMenu = new QMenu(this);
+    trayIconMenu->addAction(actShowHide);
+    trayIconMenu->setDefaultAction(actShowHide);
+    trayIconMenu->addAction(actChangeStatus);
+    trayIconMenu->addAction(actSettings);
+    trayIconMenu->addSeparator();
+    trayIconMenu->addAction(actQuit);
 
-trayIconMenu = new QMenu(this);
-trayIconMenu->addAction(actShowHide);
-trayIconMenu->setDefaultAction(actShowHide);
-trayIconMenu->addAction(actChangeStatus);
-trayIconMenu->addAction(actSettings);
-trayIconMenu->addSeparator();
-trayIconMenu->addAction(actQuit);
+    trayIcon = new QSystemTrayIcon(this);
+    trayIcon->setContextMenu(trayIconMenu);
+    trayIcon->setToolTip(tr("Quelea"));
 
-trayIcon = new QSystemTrayIcon(this);
-trayIcon->setContextMenu(trayIconMenu);
-trayIcon->setToolTip(tr("Quelea"));
+    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+            this, SLOT(iconActivated(QSystemTrayIcon::ActivationReason)));
 
-connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
-        this, SLOT(iconActivated(QSystemTrayIcon::ActivationReason)));
-
-slotChangeIcon();
-trayIcon->show();
+    slotChangeIcon();
+    trayIcon->show();
 }
 
-/*virual*/void SystemTray::closeEvent(QCloseEvent * pe)
+SystemTray::~SystemTray()
 {
-  if (trayIcon->isVisible()){
-     qc->hide();
-     }
+    delete actShowHide;
+    delete actChangeStatus;
+    delete actSettings;
+    delete actQuit;
+    delete trayIcon;
+    delete trayIconMenu;
+}
+
+void SystemTray::closeEvent(QCloseEvent * pe)
+{
+    if(trayIcon->isVisible())
+    {
+        client->hide();
+    }
 }
 
 void SystemTray::iconActivated(QSystemTrayIcon::ActivationReason reason)
 {
-switch (reason)
- {
- case QSystemTrayIcon::Trigger:
+    switch (reason)
     {
-    slotShowHide();
-    break;
+    case QSystemTrayIcon::Trigger:
+        {
+            slotShowHide();
+            break;
+        }
+    default:
+        ;
     }
- default:
-     ;
- }
 }
 
- void SystemTray::slotShowHide()
- {
- qc->setVisible(!(qc->isVisible()));
+void SystemTray::slotShowHide()
+{
+    client->setVisible(!(client->isVisible()));
 
- if (qc->isVisible())
- {
-   actShowHide->setText(tr("Скрыть"));
-   qc->activateWindow();
-   if (nMess){
-       if (receiverName=="all")
-           qc->tabWidget->setCurrentIndex(0);
-       else{
-         for (int i=0; i<=qc->tabWidget->count();i++)
-               if (receiverName==qc->tabWidget->tabText(i)){
-                   qc->tabWidget->setCurrentIndex(i);
-                   break;
-               }
-       }
-      slotChangeIcon(qc->clientStatus);
-      nMess = false;
-      receiverName="";
-   }
- }
- else
-    actShowHide->setText(tr("Показать"));
- }
+    if (client->isVisible())
+    {
+        actShowHide->setText(tr("Скрыть"));
+        client->activateWindow();
+        if (nMess){
+            if (receiverName=="all")
+                client->tabWidget->setCurrentIndex(0);
+            else{
+                for (int i=0; i<=client->tabWidget->count();i++)
+                    if (receiverName==client->tabWidget->tabText(i)){
+                    client->tabWidget->setCurrentIndex(i);
+                    break;
+                }
+            }
+            slotChangeIcon(client->clientStatus);
+            nMess = false;
+            receiverName="";
+        }
+    }
+    else
+        actShowHide->setText(tr("Показать"));
+}
 
- void SystemTray::slotShowMessage(QString receiver)
- {
-     if (receiver=="all")
-         trayIcon->showMessage(tr("Сообщение из чата"),tr("Сообщение из чата для вас"),QSystemTrayIcon::Information,3000);
-     else
-         trayIcon->showMessage(tr("Новое сообщение"),tr("Новое сообщение от ")+receiver,QSystemTrayIcon::Information,3000);
- }
+void SystemTray::slotShowMessage(QString receiverName)
+{
+    if (receiverName=="all")
+        trayIcon->showMessage(tr("Сообщение из чата"),tr("Сообщение из чата для вас"),QSystemTrayIcon::Information,3000);
+    else
+        trayIcon->showMessage(tr("Новое сообщение"),tr("Новое сообщение от ")+receiverName,QSystemTrayIcon::Information,3000);
+}
 
 
  void SystemTray::slotChangeIcon(QString status)
  {
  QString pixmapName;
- if (status=="offline"){
+ if (status=="offline")
+ {
     pixmapName ="/icon-offline.png";
     actChangeStatus->setText(tr("Подключиться"));
  }
- else if(status=="message")
-     pixmapName="/message.png";
- else {
-    pixmapName ="/icon.png";
-    actChangeStatus->setText(tr("Отключиться"));
- }
+ else
+     if(status=="message")
+         pixmapName="/message.png";
+     else
+     {
+        pixmapName ="/icon.png";
+        actChangeStatus->setText(tr("Отключиться"));
+     }
  trayIcon->setIcon(QPixmap(pixmapName));
  }
 
  void SystemTray::slotChangeStatus()
  {
 
-    if(qc->clientStatus=="offline")
-        qc->conn();
+    if(client->clientStatus=="offline")
+        client->conn();
     else
-        qc->disconn();
+        client->disconn();
  }
 
  void SystemTray::slotNewMessage(QString receiver)
  {
-     if (!qc->isVisible()){
+     if (!client->isVisible()){
         slotChangeIcon("message");
         slotShowMessage(receiver);
     }
      else
-         if (!qc->isActiveWindow())
-             QApplication::alert(qc);
+         if (!client->isActiveWindow())
+             QApplication::alert(client);
     nMess = true;
     receiverName = receiver;
 
