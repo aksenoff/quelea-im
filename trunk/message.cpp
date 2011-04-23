7dda8e3f74a06f69a46eb854fbec792aa0486f81
aksenoff.a <aksenoff.a@128f5ee6-1e47-11df-9e47-91152886f6d8>
2011-04-23 17:38:22 +0000

    Переработан механиз обмена сообщениями. Большие сообщения передаются.

--- trunk/message.cpp
+++ trunk/message.cpp
@@ -18,29 +18,11 @@ Message::Message(QTcpSocket* clientSocket)
 {
     QDataStream incomingStream(clientSocket);
     incomingStream.setVersion(QDataStream::Qt_4_6);
-    quint16 nextBlockSize = 0;
-    QVector<QString> textBlocks;
-    QString temp("");
-    incomingStream >> nextBlockSize >> code;
-    nextBlockSize = 0;
-    while(true)
-    {
-        if (!nextBlockSize) {
-            if (clientSocket->bytesAvailable() < sizeof(quint16)) {
-                break;
-            }
-            incomingStream >> nextBlockSize;
-        }
-
-        if (clientSocket->bytesAvailable() < nextBlockSize) {
-            break;
-        }
-        incomingStream >> temp;
-        textBlocks.push_back(temp);
-        nextBlockSize = 0;
-    }
-    for(QVector<QString>::const_iterator i = textBlocks.begin(); i != textBlocks.end(); ++i)
-        text += *i;
+    quint64 fullSize = 0;
+    incomingStream >> fullSize;
+    while(quint64(clientSocket->bytesAvailable()) < fullSize)
+        clientSocket->waitForReadyRead(500);
+    incomingStream >> code >> text;
 }
 
 void Message::send(QTcpSocket* socket) const
@@ -48,31 +30,8 @@ void Message::send(QTcpSocket* socket) const
     QByteArray arrBlock;
     QDataStream out(&arrBlock, QIODevice::WriteOnly);
     out.setVersion(QDataStream::Qt_4_6);
-    QString temp("");
-    out << quint16(sizeof(code)) << code;
+    out << quint64(0) << code << text;
+    out.device()->seek(0);
+    out << quint64(arrBlock.size() - sizeof(quint64));
     socket->write(arrBlock);
-    arrBlock.clear();
-    temp = text;
-    while(temp.size() > 1000)
-    {
-        out.device()->seek(0);
-        out << quint16(0) << temp.left(1000);
-        temp = temp.right(temp.size() - 1000);
-        out.device()->seek(0);
-        out << quint16(arrBlock.size() - sizeof(quint16));
-        socket->write(arrBlock);
-        arrBlock.clear();
-    }
-    if(temp.size() != 0)
-    {
-        out.device()->seek(0);
-        out << quint16(0) << temp;
-        //    QFile file("test.txt");
-        //    QTextStream stream(&file);
-        //    stream << text;
-        //    file.close();
-        out.device()->seek(0);
-        out << quint16(arrBlock.size() - sizeof(quint16));
-        socket->write(arrBlock);
-    }
 }
