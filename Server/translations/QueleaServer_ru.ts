91a15be21306b760295729d622c4204b50dc14c9
aksenoff.a <aksenoff@users.noreply.github.com>
2011-07-28 18:55:49 +0000

    Обновлен перевод.

--- Server/translations/QueleaServer_ru.ts
+++ Server/translations/QueleaServer_ru.ts
@@ -9,33 +9,37 @@
         <translation>Ошибка сервера</translation>
     </message>
     <message>
-        <location filename="../QueleaServer.cpp" line="11"/>
         <source>Can&apos;t start server</source>
+        <translation type="obsolete">Невозможно запустить сервер</translation>
+    </message>
+    <message>
+        <location filename="../QueleaServer.cpp" line="11"/>
+        <source>Unable to start server</source>
         <translation>Невозможно запустить сервер</translation>
     </message>
     <message>
-        <location filename="../QueleaServer.cpp" line="16"/>
+        <location filename="../QueleaServer.cpp" line="15"/>
         <source>Server started on</source>
         <translation>Сервер запущен на</translation>
     </message>
     <message>
-        <location filename="../QueleaServer.cpp" line="69"/>
+        <location filename="../QueleaServer.cpp" line="67"/>
         <source>connected</source>
         <translation>подключен</translation>
     </message>
     <message>
-        <location filename="../QueleaServer.cpp" line="135"/>
+        <location filename="../QueleaServer.cpp" line="132"/>
         <source>disconnected</source>
         <translation>отключен</translation>
     </message>
     <message>
-        <location filename="../QueleaServer.cpp" line="152"/>
+        <location filename="../QueleaServer.cpp" line="149"/>
         <source>Server stopped</source>
         <translation>Сервер остановлен</translation>
     </message>
     <message>
-        <location filename="../QueleaServer.cpp" line="69"/>
-        <location filename="../QueleaServer.cpp" line="135"/>
+        <location filename="../QueleaServer.cpp" line="67"/>
+        <location filename="../QueleaServer.cpp" line="132"/>
         <source>Client</source>
         <translation>Клиент</translation>
     </message>
@@ -43,69 +47,81 @@
 <context>
     <name>QueleaServerUI</name>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="12"/>
-        <location filename="../QueleaServerUI.cpp" line="74"/>
+        <location filename="../QueleaServerUI.cpp" line="13"/>
+        <location filename="../QueleaServerUI.cpp" line="76"/>
         <source>Start</source>
         <translation>Старт</translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="13"/>
-        <location filename="../QueleaServerUI.cpp" line="112"/>
+        <location filename="../QueleaServerUI.cpp" line="14"/>
+        <source>About...</source>
+        <translation>О программе...</translation>
+    </message>
+    <message>
+        <location filename="../QueleaServerUI.cpp" line="114"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="28"/>
+        <location filename="../QueleaServerUI.cpp" line="29"/>
         <source>Quelea Server</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="90"/>
+        <location filename="../QueleaServerUI.cpp" line="92"/>
         <source>Stop</source>
         <translation>Стоп</translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="114"/>
-        <source>Quelea Sever 1.0 beta 2</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../QueleaServerUI.cpp" line="115"/>
         <source>Used</source>
-        <translation>Используется</translation>
+        <translation type="obsolete">Используется</translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="115"/>
         <source>Distributed under license</source>
-        <translation>Распространяется по лицензии</translation>
+        <translation type="obsolete">Распространяется по лицензии</translation>
     </message>
     <message>
         <location filename="../QueleaServerUI.cpp" line="117"/>
+        <source>Compiled with</source>
+        <translation>Построено с использованием</translation>
+    </message>
+    <message>
+        <location filename="../QueleaServerUI.cpp" line="117"/>
+        <source>on</source>
+        <translation>Релиз</translation>
+    </message>
+    <message>
+        <location filename="../QueleaServerUI.cpp" line="118"/>
+        <source>Distributed under</source>
+        <translation>Распространяется по лицензии</translation>
+    </message>
+    <message>
+        <location filename="../QueleaServerUI.cpp" line="119"/>
         <source>Developers</source>
         <translation>Разработчики</translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="117"/>
+        <location filename="../QueleaServerUI.cpp" line="119"/>
         <source>Alexey Aksenov</source>
         <translation>Алексей Аксёнов</translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="118"/>
+        <location filename="../QueleaServerUI.cpp" line="120"/>
         <source>Roman Suhov</source>
         <translation>Роман Сухов</translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="118"/>
+        <location filename="../QueleaServerUI.cpp" line="120"/>
         <source>Alexey Topchiy</source>
         <translation>Алексей Топчий</translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="120"/>
+        <location filename="../QueleaServerUI.cpp" line="122"/>
         <source>Â©</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../QueleaServerUI.cpp" line="120"/>
+        <location filename="../QueleaServerUI.cpp" line="122"/>
         <source>Developers of</source>
         <translation>Разработчики</translation>
     </message>
